be4b54e1aef5c15338d315bf52180ce2b0a53c2d
sharmaketan14 <ks456167@gmail.com>
2024-12-10 19:44:43 +0530

    Updated Build

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,5 @@
 def call(String project, String tag, String user){
 	echo "This is building the code"
-        sh "docker build -t ${user}/${project}:${tag} ."
+        sh "docker build -t ${project}:${tag} ."
         echo "Build Complete"
 }
